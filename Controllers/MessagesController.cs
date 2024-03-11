c670d0f24fd4d3417f1cba4f2ac8eb6bf506c990
Christopher Maldonado <chmald@microsoft.com>
2024-03-11 14:54:22 -0500

    Add project files.

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DaprServiceBusMessageConsumer.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        [HttpPost("servicebus")]
+        public async Task<ActionResult<string>> processMessage()
+        {
+            string request = await new StreamReader(Request.Body).ReadToEndAsync();
+            Console.WriteLine(request);
+            return "CID" + request;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Index()
+        {
+            return "Hello";
+        }
+    }
+}
